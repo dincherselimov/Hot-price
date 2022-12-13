060b79c4e72fa60ef6965dc0c6cdedabcd1a71e6
Dincher <dinchitoo@gmail.com>
2022-12-13 21:31:48 +0200

    adsad

--- src/js/app.js
+++ src/js/app.js
@@ -3,7 +3,7 @@ import "../scss/app.scss";
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
   
-   let element  = document.getElementsByClassName('hot');
+   let element  = document.querySelectorAll('hot');
 
       for(let i=0; i <element.length; i++){
         element[0].innerHTML = element.innerHTML + " 🔥";
